48d6d2069919d240ea51380feb1cc4bf85a69758
ilham wahiduddin <ilham.wahiduddin@gmail.com>
2025-10-25 17:52:43 +0700

    10 Hapus data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -59,5 +60,89 @@ class PostController extends Controller
 
         // redirect to posts.index
         return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Disimpan!']);
+    }
+
+    /**
+     * edit
+     * 
+     * @param mixed $post
+     * @return void
+     */
+    public function edit(string $id)
+    {
+        // get data post
+        $post = Post::findOrFail($id);
+        
+        // render view pada posts.edit
+        return view('posts.edit', compact('post'));
+    }
+
+    /**
+     * store
+     * 
+     * @param Request $request
+     * @param mixed $post
+     * @return void
+     */
+    public function update(Request $request, string $id)
+    {
+        // validate request
+        $request->validate([
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title' => 'required|string|min:5|max:255',
+            'content' => 'required|string|min:10',
+        ]);
+
+        // get data post
+        $post = Post::findOrFail($id);
+
+        // check if  image is uploaded
+        if ($request->hasFile('image')) {
+
+            // delete old image
+            Storage::delete('public/posts/'.$post->image);
+
+            // upload new image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            // update post with new image
+            $post->update([
+                'image' => $image->hashName(),
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+        } else {
+            // update post without image
+            $post->update([
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+        }
+
+        // redirect to posts.index
+        return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Diubah!']);
+    }
+
+    /**
+     * destroy
+     * 
+     * @param mixed $post
+     * @return void
+     */
+    public function destroy(string $id)
+    {
+
+        // get data post
+        $post = Post::findOrFail($id);
+
+        // delete image
+        Storage::delete('public/posts/'.$post->image);
+
+        // delete post
+        $post->delete();
+
+        // redirect to posts.index
+        return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Dihapus!']);
     }
 }
