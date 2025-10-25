4ec1ee66f480fce32b01efdf513915f63a5e6959
ilham wahiduddin <ilham.wahiduddin@gmail.com>
2025-10-25 17:24:29 +0700

    9 INSERT DATA Pada #laravel

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,4 +20,44 @@ class PostController extends Controller
         // render view pada posts.index
         return view('posts.index', compact('posts'));
     }
+
+    /**
+     * create
+     * 
+     * @return void
+     */
+    public function create()
+    {
+        // render view pada posts.create
+        return view('posts.create');
+    }
+
+    /**
+     * store
+     * 
+     * @param Request $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        // validate request
+        $request->validate([
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title' => 'required|string|min:5|max:255',
+            'content' => 'required|string|min:10',
+        ]);
+
+        // upload image
+        $image = $request->file('image');
+        $image ->storeAs('public/posts', $image->hashName());
+        // create post
+        Post::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'content' => $request->content,
+        ]);
+
+        // redirect to posts.index
+        return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Disimpan!']);
+    }
 }
